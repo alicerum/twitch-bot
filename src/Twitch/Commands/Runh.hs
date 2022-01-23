d8fa9f2da15286be4b216671d5888f1e952746b4
Alice Rum <alice.rum@redhat.com>
2022-01-23 17:25:44 +0100

    Long interpretered operation now times out
    Another thread starts and tries to kill the interpreter thread after
    a timeout. In case it succeeds, it means code has been taking too
    long (2 seconds) and should be killed.

--- src/Twitch/Commands/Runh.hs
+++ src/Twitch/Commands/Runh.hs
@@ -3,21 +3,38 @@ module Twitch.Commands.Runh (
 ) where
 
 import Control.Monad (join)
+import Control.Concurrent
+import Control.Applicative ((<|>))
 import System.Directory.Internal.Prelude (timeout, fromMaybe)
 import Language.Haskell.Interpreter (as, runInterpreter, setImports, interpret)
+import Control.Exception (evaluate)
+import Control.DeepSeq (force)
+
+runWithTimeout :: String -> IO (Maybe String)
+runWithTimeout s = do
+    mvar <- newEmptyMVar
+    threadId <- forkIO  $ do
+        res <- interp s
+        newRes <- evaluate $ force res
+        putMVar mvar newRes
+
+    threadKiller <- forkIO $ do
+        threadDelay $ 2 * (10^6) -- 2 seconds
+        killThread threadId
+        putMVar mvar Nothing
+
+    res <- takeMVar mvar
+    killThread threadKiller
+    return res
 
 errToMaybe :: Show e => Either e String -> Maybe String
 errToMaybe (Left e) = Just $ "error " <> (unwords . lines) (show e)
 errToMaybe (Right a) = Just a
 
-runWithTimeout :: String -> IO (Maybe String)
-runWithTimeout s = fmap join . timeout (2 * (10 ^ 6)) $ interp s
-
 runHString :: String -> IO String
 runHString s = do
     res <- runWithTimeout s
-    return $ maybe "unknown errror" (take 200) (res <* Just "Timed out")
-
+    return $ maybe "unknown errror" (take 200) (res <|> Just "Timed out")
 
 interp :: String -> IO (Maybe String)
 interp s = fmap errToMaybe $ runInterpreter $ do
